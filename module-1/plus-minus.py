e24d874429c44b3ddb4e63881a0bcbb6a8e5d5ad
bwho <barrett.olafson@gmail.com>
2023-10-13 21:34:39 -0400

    initial submissions for module 1 homework assignment

---
+++ module-1/plus-minus.py
@@ -0,0 +1,11 @@
+#Gather first number
+print('Enter a number:', end=' ')
+num1 = int(input())
+
+#Gather second number
+print('Enter another number:', end=' ')
+num2 = int(input())
+
+#Calculate & display all the things
+print('Sum:', num1 * num2)
+print('Difference:', num1 / num2)
