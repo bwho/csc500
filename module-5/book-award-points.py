bdc49e2126e3ba4abaa16b8521decbd530b9bb27
bwho <barrett.olafson@gmail.com>
2023-11-15 21:02:56 -0500

    accidentially forgot comments for book-award-points.py, just saw the git status and so adding them now -- I think I need to close out my files once I've completed editing them...

--- module-5/book-award-points.py
+++ module-5/book-award-points.py
@@ -1,8 +1,9 @@
+#Gather the number of books purchased for the month from user input
 print("Enter number of books purchased this month: ")
 num_books = int(input())
 
-award_points = 0 
-
+#If-else logic branches for the awards granted for each level of books
+#purchased
 if (num_books >= 0) and (num_books < 2):
 	award_points = 0
 elif (num_books >= 2) and (num_books < 4):
@@ -14,4 +15,5 @@ elif (num_books >= 6) and (num_books < 8):
 elif (num_books >= 8):
 	award_points = 60
 
+#Output the points awarded based on the books purchased
 print(f"Number of points awarded for this month: {award_points}")
